19d8d1accfc18fdaffd638193f41942b3264530d
JordanIschard <jordan_is@hotmail.fr>
2024-05-22 08:38:47 +0200

    ListLevel done

--- theories/List/ListLevel.v
+++ theories/List/ListLevel.v
@@ -1,5 +1,6 @@
-From Coq Require Import Orders Lia RelationPairs Bool.Bool Structures.EqualitiesFacts.
+From Coq Require Import Orders Lia RelationPairs Bool.Bool Structures.EqualitiesFacts Lists.List.
 From DeBrLevel Require Import LevelInterface Level.
+Import ListNotations.
 
 Module IsLvlListETWL (E : IsLvlETWL) <: IsLvlETWL.
 
@@ -12,10 +13,10 @@ Definition t := list E.t.
 Definition eq := @Logic.eq (list E.t).
 
 Definition shift (lb k : Level.t) (t : t) := 
- List.map (E.shift lb k) t.
+ map (E.shift lb k) t.
 
 Definition valid (lb : Level.t) (t : t) := 
- forall x, List.In x t -> E.valid lb x.
+ forall x, In x t -> E.valid lb x.
 
 End definition.
 
@@ -84,7 +85,7 @@ End equality.
 Section shift.
 
 Variable lb k k' k'' : Level.t.
-Variable t : t.
+Variable t t' : t.
 
 Lemma shift_refl : eq (shift lb 0 t) t.
 Proof.
@@ -118,10 +119,87 @@ Proof.
   apply E.eq_leibniz; now apply E.shift_unfold_1. 
 Qed.
 
+Lemma shift_nil_spec: eq (shift lb k nil) nil.
+Proof. unfold eq, shift; now simpl. Qed.
+
+Lemma shift_cons_spec v: 
+  eq (shift lb k (v :: t)) ((E.shift lb k v) :: (shift lb k t)).
+Proof. unfold eq, shift; now simpl. Qed.
+
+Lemma shift_app_spec: 
+  eq (shift lb k (t ++ t')) ((shift lb k t) ++ (shift lb k t')).
+Proof.
+  unfold eq, shift. apply map_app.
+Qed.
+
+Lemma shift_in_spec v: In v t <-> In (E.shift lb k v) (shift lb k t).
+Proof.
+  split. clear k' k'' t'.
+  - revert v. induction t; intros; simpl in *; auto.
+    destruct H; subst; auto.
+  - revert v. induction t; intros; simpl in *; auto.
+    destruct H; subst; auto.
+    left. apply E.eq_leibniz. 
+    rewrite E.shift_eq_iff. now rewrite H.
+Qed.
+
+Lemma shift_in_e_spec v:
+  In v (shift lb k t) -> 
+  exists v', v = (E.shift lb k v') /\ In (E.shift lb k v') (shift lb k t).
+Proof.
+  clear k' k'' t'. revert v; induction t; simpl; intros.
+  - inversion H.
+  - destruct H; subst.
+    -- exists a. split; auto.
+    -- apply IHt0 in H as [v' [Heq HIn]]; subst.
+       exists v'; split; auto.
+Qed.
+
+Lemma shift_notin_spec v : ~ In v t <-> ~ In (E.shift lb k v) (shift lb k t).
+Proof.
+  intros; split; unfold not; intros; apply H.
+  - now rewrite <- shift_in_spec in H0.
+  - now rewrite <- shift_in_spec.
+Qed.
+
 End shift.
 
 (** *** Valid *)
 Section valid.
+
+Lemma valid_unfold lb s:
+ valid lb s <-> (forall v, List.In v s -> E.valid lb v).
+Proof. now unfold valid. Qed.
+
+Lemma valid_nil_spec lb: valid lb [].
+Proof. unfold valid; intros x c. inversion c. Qed.
+
+Lemma valid_cons_spec lb v s:
+  valid lb (v :: s) <-> E.valid lb v /\ valid lb s.
+Proof.
+  unfold valid; split; intros.
+  - split.
+    -- apply H. simpl; now left.
+    -- intros x HIn.
+       apply H. simpl; now right.
+  - destruct H,H0; subst; auto.
+Qed.
+
+Lemma valid_app_spec lb s s':
+  valid lb (s ++ s') <-> valid lb s /\ valid lb s'.
+Proof.
+  unfold valid; split; intros.
+  - split; intros.
+    -- apply H.
+       apply in_or_app; now left.
+    -- apply H.
+       apply in_or_app; now right.
+  - apply in_app_or in H0.
+    destruct H, H0; auto.
+Qed.
+
+Lemma valid_in_spec lb t v : valid lb t -> In v t -> E.valid lb v.
+Proof. unfold valid; intros; now apply H. Qed.
 
 Lemma valid_weakening : forall k k' (t : t), 
   (k <= k') -> valid k t -> valid k' t.
@@ -134,16 +212,34 @@ Qed.
 Lemma shift_preserves_valid : forall k k' t, 
   valid k t -> valid (k + k') (shift k k' t).
 Proof.
-Admitted.
+  intros k k' t. induction t; simpl; intros.
+  - apply valid_nil_spec.
+  - rewrite valid_cons_spec in *; 
+    destruct H; split; auto.
+    now apply E.shift_preserves_valid.
+Qed.
 
 Lemma shift_preserves_valid_1 : forall lb k k' t, 
   valid k t -> valid (k + k') (shift lb k' t).
-Proof. Admitted.
+Proof.
+  intros lb k k' t. induction t; simpl; intros.
+  - apply valid_nil_spec.
+  - rewrite valid_cons_spec in *; 
+    destruct H; split; auto.
+    now apply E.shift_preserves_valid_1.
+Qed.
 
 Lemma shift_preserves_valid_2 : forall lb lb' k k' t,  
   k <= k' -> lb <= lb' -> k <= lb -> k' <= lb' ->
   k' - k = lb' - lb ->  valid lb t -> valid lb' (shift k (k' - k) t).
-Proof. Admitted.
+Proof.
+  intros lb lb' k k' t Hle Hle1 Hle2 Hle3 Heq.
+  induction t; simpl; intros.
+  - apply valid_nil_spec.
+  - rewrite valid_cons_spec in *; 
+    destruct H; split; auto.
+    now apply E.shift_preserves_valid_2 with lb.
+Qed.
 
 Lemma shift_preserves_valid_3 : forall lb lb' t, 
   lb <= lb' -> valid lb t -> valid lb' (shift lb (lb' - lb) t).
