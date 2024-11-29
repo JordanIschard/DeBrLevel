3b5a14ca4cee302e629d4161d49a0ba3eb72ca24
JordanIschard <jordan_is@hotmail.fr>
2024-11-29 15:04:41 +0100

    fix index link

--- doc/resources/coqdocjs.js
+++ doc/resources/coqdocjs.js
@@ -320,7 +320,7 @@ function toggleProofs(){
     var url = "/" + window.location.pathname;
     var basename = url.substring(url.lastIndexOf('/')+1, url.lastIndexOf('.'));
     if (basename === "toc") {document.title = "Table of Contents";}
-    else if (basename === "indexpage") {document.title = "Index";}
+    else if (basename === "index") {document.title = "Index";}
     else {document.title = basename;}
   }
   
@@ -365,7 +365,7 @@ function fixTitle(){
   var url = "/" + window.location.pathname;
   var basename = url.substring(url.lastIndexOf('/')+1, url.lastIndexOf('.'));
   if (basename === "toc") {document.title = "Table of Contents";}
-  else if (basename === "indexpage") {document.title = "Index";}
+  else if (basename === "index") {document.title = "Index";}
   else {document.title = basename;}
 }
 
